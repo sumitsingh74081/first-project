51366bbfe7fd815e0cf858688d97084bb796f91f
sumitsingh74081 <Sumit.hangingpanda@gmail.com>
2021-10-26 17:36:00 +0530

    make change in first push

--- project2/src/App.js
+++ project2/src/App.js
@@ -9,6 +9,7 @@ export default function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>first push</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
